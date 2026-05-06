c210e2ef2f7167dff3679c5318d11179565246a4
Victor Schweitz <victorschweitz@hotmail.com>
2026-05-06 11:00:37 +0200

    Nothing

--- frontend/src/app/memberships/page.tsx
+++ frontend/src/app/memberships/page.tsx
@@ -1,19 +1,20 @@
-import MembershipCard from "@/src/components/cards/MembershipCard";
+import MembershipCard from "@/src/components/cards/MembershipCard"
 import Navigation from "@/src/components/navigation/Navigation";
 
-const membership = {
-  id: "1",
-  name: "Premium",
-  price: 299,
-  description: "Unlimited washes and discounts",
-};
+const membership =
+{
+    id: '1',
+    name: 'What the fuck',
+    price: 100,
+    description: 'Where the hell is Sandra?'
+}
 
-export default function Memberships() {
-  return (
-    <>
-      <Navigation />
-
-      <MembershipCard membership={membership} />
-    </>
-  );
+export default function Memberships()
+{
+    return (
+        <>
+            <Navigation />
+            <MembershipCard membership={membership} />
+        </>
+    )
 }
