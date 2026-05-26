78bae009a52ce68e6d574213ac14c27ed33ad7a2
fatou Jallow <128003934+fantadiallo@users.noreply.github.com>
2026-05-26 11:52:54 +0200

    change: footer in each page

--- frontend/src/app/forgot-password/page.tsx
+++ frontend/src/app/forgot-password/page.tsx
@@ -2,6 +2,7 @@ import ForgotPasswordForm from "@/src/components/auth/ForgotPasswordForm";
 import PageContainer from "@/src/components/containers/PageContainer";
 import Navigation from "@/src/components/navigation/Navigation";
 import Layout from "../layout";
+import Footer from "@/src/components/layout/Footer";
 
 
 export default function ForgotPasswordPage() {
@@ -11,6 +12,10 @@ export default function ForgotPasswordPage() {
       <PageContainer>
       <ForgotPasswordForm />
     </PageContainer>
+    <Footer
+            address="Dynamovej 4, 2860 Søborg"
+            phone="+45 70 70 70 70"
+          />
     </Layout>
   );
 }
